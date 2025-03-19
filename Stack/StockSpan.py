6de8811eae330793ef6b1d63fd929b325916b61b
Sibil Sarjam Soren <sibil.sorenofficial@gmail.com>
2025-03-19 20:04:02 +0530

    simpler steps added to StockSpan problem

--- Stack/StockSpan.py
+++ Stack/StockSpan.py
@@ -3,20 +3,34 @@ nums = [100,80,60,70,60,75,85]
 def StockSpan(arr):
     stack = []
     result = []
-    for index, value in enumerate(arr):
-        if len(stack) == 0:
-            result.append(index - -1)
-        elif len(stack) != 0 and stack[-1]["value"] > value:
-            result.append(index - stack[-1]["index"])
-        elif len(stack) != 0 and stack[-1]["value"] <= value:
-            while len(stack) > 0 and stack[-1]["value"] <= value:
-                stack.pop()
-            if len(stack) == 0:
-                result.append(index - -1)
-            else:
-                result.append(index - stack[-1]["index"])
-        stack.append({"index":index,"value":value})
-    return result
 
+    
+
+    # while stack and stack[-1][0]
+    # for index, value in enumerate(arr):
+    #     if len(stack) == 0:
+    #         result.append(index - -1)
+    #     elif len(stack) != 0 and stack[-1]["value"] > value:
+    #         result.append(index - stack[-1]["index"])
+    #     elif len(stack) != 0 and stack[-1]["value"] <= value:
+    #         while len(stack) > 0 and stack[-1]["value"] <= value:
+    #             stack.pop()
+    #         if len(stack) == 0:
+    #             result.append(index - -1)
+    #         else:
+    #             result.append(index - stack[-1]["index"])
+    #     stack.append({"index":index,"value":value})
+    # return result
+
+    ## Another way of writing the same problem.
+
+    for value in arr:
+        res = 1
+        while stack and stack[-1][0] <= value:
+            res += stack.pop()[1]
+        stack.append([value,res])
+        result.append(res)
+    
+    return result
 
 print(StockSpan(nums))
