1912b7d3f458b99166dcd2a50078a327fede88dc
Sibil Sarjam Soren <sibil.sorenofficial@gmail.com>
2025-03-19 16:10:29 +0530

    Stock plan approach understood

---
+++ Stack/StockPlan.py
@@ -0,0 +1,22 @@
+nums = [100,80,60,70,60,75,85]
+
+def StockPlan(arr):
+    stack = []
+    result = []
+    for index, value in enumerate(arr):
+        if len(stack) == 0:
+            result.append(index - -1)
+        elif len(stack) != 0 and stack[-1]["value"] > value:
+            result.append(index - stack[-1]["index"])
+        elif len(stack) != 0 and stack[-1]["value"] <= value:
+            while len(stack) > 0 and stack[-1]["value"] <= value:
+                stack.pop()
+            if len(stack) == 0:
+                result.append(index - -1)
+            else:
+                result.append(index - stack[-1]["index"])
+        stack.append({"index":index,"value":value})
+    return result
+
+
+print(StockPlan(nums))
