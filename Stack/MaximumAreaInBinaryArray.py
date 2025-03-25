7a03bda64c304b0d389a9bb99f542f78ac0cbac7
Sibil Sarjam Soren <sibil.sorenofficial@gmail.com>
2025-03-25 10:32:29 +0530

    Minor changes

---
+++ Stack/MaximumAreaInBinaryArray.py
@@ -0,0 +1,82 @@
+def NSL(arr):
+    stack = []
+    result = []
+    for i,value in enumerate(arr):
+        if len(stack) == 0:
+            result.append(-1)
+        elif len(stack) != 0 and stack[-1][0] < value:
+            result.append(stack[-1][1])
+        elif len(stack) != 0 and stack[-1][0] >= value:
+            while len(stack) > 0 and stack[-1][0] >= value:
+                stack.pop()
+            if len(stack) == 0:
+                result.append(-1)
+            else:
+                result.append(stack[-1][1])
+        stack.append([value,i])
+    return result
+
+
+def NSR(arr):
+    stack = []
+    result = []
+    for i,value in enumerate(reversed(arr)):
+        reverse_index = len(heights) - 1 - i
+
+        if len(stack) == 0:
+            result.append(len(arr))
+        elif len(stack) != 0 and stack[-1][0] < value:
+            result.append(stack[-1][1])
+        elif len(stack) != 0 and stack[-1][0]  >= value:
+            while len(stack) > 0 and stack[-1][0] >= value:
+                stack.pop()
+            if len(stack) == 0:
+                result.append(len(arr))
+            else:
+                result.append(stack[-1][1])
+        # print(value,reverse_index)
+        stack.append([value,reverse_index])
+    
+    result.reverse()
+    return result
+
+heights = [2,4]
+
+
+def maxArea(arr):
+    left = NSL(arr)
+    right = NSR(arr)
+
+    # print(left,right)
+
+    area = []
+    for i,value in enumerate(arr):
+        area.append((right[i] - left[i] - 1) * value)
+
+    return abs(max(area))
+
+
+nums = [[0,1,1,0],
+       [1,1,1,1],
+       [1,1,1,1],
+       [1,1,0,0]]
+
+def maxAreaInBinaryArray(arr):
+    n = len(arr)
+    area = []
+
+    histograms=[0] * len(arr[0])
+
+    for i in range(n):
+        for j in range(len(arr[i])):
+            if arr[i][j] == 0:
+                histograms[j] = 0 
+            else:
+                histograms[j] += arr[i][j]
+            area.append(maxArea(histograms))
+
+
+    return max(area)
+
+print(maxAreaInBinaryArray(nums))
+
