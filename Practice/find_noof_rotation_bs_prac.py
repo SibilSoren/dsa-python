e71fe1cd73bacc048a693a8bc5f2e15c26e046b1
Sibil Sarjam Soren <sibil.sorenofficial@gmail.com>
2025-04-11 20:53:20 +0530

    practised: number of rotation in a sorted array

---
+++ Practice/find_noof_rotation_bs_prac.py
@@ -0,0 +1,20 @@
+def findRotation(arr):
+    start = 0
+    end = len(arr) - 1
+    n = len(arr)
+    while start <= end:
+        if arr[start] <= arr[end]:
+            return start
+        mid = int(start + ((end - start)/2))
+        prev = (mid + n - 1) % n
+        next = (mid + 1) % n
+        if arr[mid] < arr[prev] and arr[mid] <  arr[next]:
+            return mid
+        elif arr[start] <= arr[mid]:
+            start = mid + 1
+        elif arr[mid] <= arr[end]:
+            end = mid - 1
+
+nums = [11,14,15,18,2,4,6,8]
+
+print(findRotation(nums))
